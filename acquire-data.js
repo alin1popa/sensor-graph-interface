25a8c05c253e6a08350ad10d3f41d1438eb33ffb
alin1popa <alin1popa@gmail.com>
2017-02-17 22:20:58 +0200

    data crossroad

--- acquire-data.js
+++ acquire-data.js
@@ -1,8 +1,4 @@
 
-//const TEMPERATURE = 1;
-//const PRESSURE = 2;
-//const HUMIDITY = 3;
-
 /* Detects the correct data source
  * Acquires and returns the correct data
  * Parameters:	sensorid = id string
@@ -11,5 +7,23 @@
  * Return value: data[]
  */
  function jsDataCrossroad(sensorid, measurement, from, to){
+	//TODO AJAX request from API
+	
+	//TODO 
+	//callback function on success:
+	// - set config struct
+	// - data process
+	// - data notify
+ }
  
+ /* Custom data processing
+  * depending on the config struct
+  * Parameters: data, configuration struct
+  * Return value: true
+  */
+ function dataCustomProcessing(data, config){
+	
+	//TODO custom processing
+	
+	return true;
  }
