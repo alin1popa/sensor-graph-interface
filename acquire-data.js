886a38a9e8af3cd9fdab000c6fb56eb0c03db446
Mihai-Alexandru Stoica <stoicama@yahoo.com>
2017-02-19 14:50:13 +0200

    part1

--- acquire-data.js
+++ acquire-data.js
@@ -6,8 +6,24 @@
 				from, to = Unix time value
  * Return value: data[]
  */
- function jsDataCrossroad(sensorid, measurement, from, to){
+ function jsDataCrossroad(sensorid, measurement, fromx, tox){
 	//TODO AJAX request from API
+	
+	$.ajax({
+		type: 'GET',
+		url: "http://data.uradmonitor.com/api/v1/devices/" + sensorid + "/" + measurement + "/" + (Date.now() - fromx) + "/" + (Date.now() - tox),
+		dataType: 'json',
+		headers: { 'Content-Type' : 'text/plain',
+			'X-User-id':'www',
+			'X-User-hash':'global' },
+		success: function(data) { 
+			// apply factors: 0.01 cpm to usv/h conversion factor for the SI29BG tube
+			if (sensor == "cpm")
+				dataCustomProcessing(data, "cpm");
+			return data;
+		},
+		async: true
+	});
 	
 	//TODO 
 	//callback function on success:
@@ -21,9 +37,11 @@
   * Parameters: data, configuration struct
   * Return value: true
   */
- function dataCustomProcessing(data, config, g){
+ function dataCustomProcessing(data, config){
 	//TODO custom processing
-	
+	if (config == "cpm")
+		for (var i = 0; i < data.length; i++)
+			data[i][cpm] *= 0.01;
 	return true;
  }
  
